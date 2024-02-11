24e41d2c0577822f077bb1d4d3e0436f2250afc9
Ashutosh Singh <singh.ashuto@northeastern.edu>
2024-02-11 18:16:43 -0500

    Added integration test for user

--- src/test/java/com/csye6225/cloud/controller/HealthControllerTest.java
+++ src/test/java/com/csye6225/cloud/controller/HealthControllerTest.java
@@ -13,7 +13,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class HealthControllerTest {
