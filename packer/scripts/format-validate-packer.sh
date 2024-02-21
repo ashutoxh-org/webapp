eac737b8d01f54b2ea4d6be56b8478456e74e03a
Ashutosh Singh <singh.ashuto@northeastern.edu>
2024-02-20 22:06:41 -0500

    Added workflow for packer lint

--- packer/scripts/format-validate-packer.sh
+++ packer/scripts/format-validate-packer.sh
@@ -15,18 +15,35 @@ if [ -z "$GITHUB_ACTIONS" ]; then
   echo "Local validate"
   find . -type f -name "*.pkr.hcl" -exec packer validate -var-file="packer/environments/dev.pkrvars.hcl" {} \;
 else
+  if [ -z "$LINT" ]; then
   # If running in GitHub Actions, use -var options with secrets
-  echo "GitHub Actions validate"
-  find . -type f -name "*.pkr.hcl" -exec packer validate \
-    -var "environment=${ENVIRONMENT}" \
-    -var "project_id=${PROJECT_ID}" \
-    -var "deployment_zone=${DEPLOYMENT_ZONE}" \
-    -var "vpc_network=${VPC_NETWORK}" \
-    -var "source_image_family=${SOURCE_IMAGE_FAMILY}" \
-    -var "image_family=${IMAGE_FAMILY}" \
-    -var "ssh_username=${SSH_USERNAME}" \
-    -var "machine_type=${MACHINE_TYPE}" \
-    {} \;
+    echo "GitHub Actions validate during build"
+    find . -type f -name "*.pkr.hcl" -exec packer validate \
+      -var "environment=${ENVIRONMENT}" \
+      -var "project_id=${PROJECT_ID}" \
+      -var "deployment_zone=${DEPLOYMENT_ZONE}" \
+      -var "vpc_network=${VPC_NETWORK}" \
+      -var "source_image_family=${SOURCE_IMAGE_FAMILY}" \
+      -var "image_family=${IMAGE_FAMILY}" \
+      -var "ssh_username=${SSH_USERNAME}" \
+      -var "machine_type=${MACHINE_TYPE}" \
+      {} \;
+  else
+    echo "GitHub Actions validate during lint"
+    mkdir -p target
+    echo "This is a fake JAR file." > target/CloudNativeApplication-0.0.1-SNAPSHOT.jar
+    find . -type f -name "*.pkr.hcl" -exec packer validate \
+      -var "environment=${ENVIRONMENT}" \
+      -var "project_id=${PROJECT_ID}" \
+      -var "deployment_zone=${DEPLOYMENT_ZONE}" \
+      -var "vpc_network=${VPC_NETWORK}" \
+      -var "source_image_family=${SOURCE_IMAGE_FAMILY}" \
+      -var "image_family=${IMAGE_FAMILY}" \
+      -var "ssh_username=${SSH_USERNAME}" \
+      -var "machine_type=${MACHINE_TYPE}" \
+      {} \;
+    rm -rf target
+  fi
 fi
 
 
